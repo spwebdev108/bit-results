28e819c65ce9a1994811e294192f93eb77ab4c44
Avinash Som <avicode23@gmail.com>
2024-12-19 23:52:41 +0530

    add new

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require('express')
+const app = express()
 const port = 8080;
 const result = require("./models/res.js")
 require('dotenv').config();
 const mongoose = require("mongoose");
 const path = require("path")
-
 const dbUrl= process.env.ATLASDB_URI;
+
 
 main().then(() => {
   console.log("connect to db");
@@ -21,20 +22,19 @@ app.set("view engine ", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 app.use(express.static(path.join(__dirname, "/public")));
 
-
 app.get('/', (req, res) => {
-    res.render('./results/home.ejs')
+    res.send('Hello World!')
 })
 
 app.get("/results", async (req, res) => {
-  let allResults = await result.find({});
-  res.render("./results/index.ejs", { allResults })
+    let allResults = await result.find({});
+    res.render("./results/index.ejs", { allResults })
 })
 
 app.get("/results/:id", async (req, res) => {
-  let { id } = req.params;
-  const r = await result.findById(id);
-  res.render("./results/show.ejs", { r});
+    let { id } = req.params;
+    const r = await result.findById(id);
+    res.render("./results/show.ejs", { r});
 })
 app.get("/contact", (req, res) => {
   res.render("./results/contact.ejs");
